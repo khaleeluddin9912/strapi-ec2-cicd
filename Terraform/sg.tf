603e6c96ba3ffc70d5ba7fe9441c2bc42d108056
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-11 17:14:53 +0530

    Modify security group rules for Strapi EC2
    Updated security group rules for Strapi EC2, swapping SSH and Strapi ports.

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -1,28 +1,40 @@
+# sg.tf - Security Group for Strapi EC2
+
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow SSH and Strapi ports"
+  name        = "strapi_sg"
+  description = "Security group for Strapi EC2"
   vpc_id      = data.aws_vpc.default.id
 
+  # Allow HTTP for Strapi (port 1337)
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Strapi port"
     from_port   = 1337
     to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow SSH access (port 22)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "StrapiSG"
   }
 }
