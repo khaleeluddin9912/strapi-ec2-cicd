00b2dd03554affaaf84830858c41eaa67cb3e8bf
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-12-11 18:12:11 +0530

    Add sum changes
    Updated user_data to pass ecr_repo along with image_tag.

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -3,13 +3,19 @@ resource "aws_key_pair" "strapi_key" {
   public_key = file("${path.cwd}/${var.key_name}.pub")
 }
 
+# Create EC2 instance
 resource "aws_instance" "strapi_ec2" {
-  ami                    = "ami-02b8269d5e85954ef"
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.strapi_key.key_name
-  security_groups        = [aws_security_group.strapi_sg.name]
+  ami                         = "ami-02b8269d5e85954ef"
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.strapi_key.key_name
+  security_groups             = [aws_security_group.strapi_sg.name]
   associate_public_ip_address = true
-  user_data              = templatefile("user-data.sh", { image_tag = var.image_tag })
+
+  # Pass both image_tag and ecr_repo to the template
+  user_data = templatefile("user-data.sh.tpl", {
+    image_tag = var.image_tag,
+    ecr_repo  = var.ecr_repo
+  })
 
   tags = {
     Name = "Strapi-EC2"
